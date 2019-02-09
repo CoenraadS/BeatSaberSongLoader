b809d7fe0ef9f2452d1480535bdd30cb3bbe819e
Coenraad Stijne <coenraad.stijne@gmail.com>
2019-02-09 15:55:43 +0100

    dicts and sets

--- SongLoaderPlugin/Utils.cs
+++ SongLoaderPlugin/Utils.cs
@@ -7,7 +7,7 @@ namespace SongLoaderPlugin
 {
 	public static class Utils
 	{
-		public static TEnum ToEnum<TEnum>(this string strEnumValue, TEnum defaultValue)
+		public static TEnum ToEnum<TEnum>(string strEnumValue, TEnum defaultValue)
 		{
 			if (!Enum.IsDefined(typeof(TEnum), strEnumValue))
 				return defaultValue;
